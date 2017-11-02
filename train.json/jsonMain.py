4980c3342d3f27ab3dbb3a665be3843b64c86d66
Guilherme Defalque <guilhermedefalque@gmail.com>
2017-11-02 16:07:30 -0300

    Arquivo de entrada separado em xs, y e ids. Problema de estouro de memória devido a grande quantidade de entradas;

--- train.json/jsonMain.py
+++ train.json/jsonMain.py
@@ -1,10 +1,20 @@
 import json
 from pprint import pprint
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.colors import ListedColormap
+from sklearn import neighbors, datasets
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors import NearestNeighbors
 
-def main():
+def leArquivoJson():
     with open('train.json') as data_file:
         data = json.load(data_file)
-    #print(data[0])
+    print(len(data))
+    return data
+
+def criarTodosOsIngrediente(data):
     ingredientes = []
     for j in data:
         ingredientes.append(j['ingredients'])
@@ -12,20 +22,41 @@ def main():
     for i in range(0, len(ingredientes)):
         for j in range(0, len(ingredientes[i])):
             todosOsIngredientes.append((ingredientes[i][j]).lower())
-    #print(len(ingredi))
+    # print(len(ingredi))
     todosOsIngredientes = set(todosOsIngredientes)
-    #print(len(ingredi))
+    return todosOsIngredientes
 
-    variaveis = []
-
-    for i in data:
-        aux = ""
-        aux = aux + str(i['id']) + ", "
+def criarXSYIDS(dicionarioDeJson, todosOsIngredientes):
+    id = []
+    xs = []
+    y = []
+    for i in dicionarioDeJson:
+        xIngredientes = []
+        id.append(str(i['id']))
         for j in todosOsIngredientes:
-            aux = aux + ('1, ' if j in i['ingredients'] else '0, ')
-        aux = aux + i['cuisine'] + ", \n"
-        variaveis.append(aux)
-        print(aux)
+            if j in i['ingredients']:
+                xIngredientes.append('1')
+            else:
+                xIngredientes.append('0')
+        xs.append(xIngredientes)
+        y.append(i['cuisine'])
+    return xs, y, id
+
+def main():
+    dicionarioDeJson = leArquivoJson()
+    #print(dicionarioDeJson[0])
+    todosOsIngredientes = criarTodosOsIngrediente(dicionarioDeJson)
+    print(len(todosOsIngredientes))
+    #print(len(todosOsIngredientes))
+    xs, y, id = criarXSYIDS(dicionarioDeJson, todosOsIngredientes)
+    #print(id[0])
+    print(xs[0])
+    print(y[0])
+    #X_train, X_test, y_train, y_test = train_test_split(xs, y, test_size=0.3, random_state=0)
+    clf = neighbors.KNeighborsClassifier(15, weights='uniform')
+    clf.fit(xs, y)
+    maiorScore = clf.score(xs, y)
+    print("Melhores resultados: Weight: uniform, k = 15, Score: %f" % (maiorScore))
     return
 
 
